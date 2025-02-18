2b070eb5e29311c5f10a7948ba8ccf86d85bbf00
Toka Ashraf <122158105+tokaashraf1@users.noreply.github.com>
2025-02-18 13:10:10 -0800

    store

---
+++ src/redux-toolkit/store.jsx
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import taskSlice from './slices/taskSlice';
+export const store=configureStore({
+    reducer:{
+       task:taskSlice 
+    }
+})
