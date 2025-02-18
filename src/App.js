dcba1001efa07595c6760a7d734c2ddf8797bfd3
Toka Ashraf <122158105+tokaashraf1@users.noreply.github.com>
2025-02-18 13:37:40 -0800

    modifying reducer name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className="App">
-  
+  <TaskList/>
     </div>
   );
 }
--- src/redux-toolkit/store.jsx
+++ src/redux-toolkit/store.jsx
@@ -2,6 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import taskSlice from './slices/taskSlice';
 export const store=configureStore({
     reducer:{
-       task:taskSlice 
+       tasks:taskSlice 
     }
 })
